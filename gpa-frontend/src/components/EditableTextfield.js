4f2d478274845c7c0d27bf4bc0f2bafda909c93b
Pedro <pedrocpt2001@icloud.com>
2023-04-24 21:27:43 -0400

    Added style

---
+++ gpa-frontend/src/components/EditableTextfield.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const EditableTextField = styled.input`
+  border: none;
+  background: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  outline: none;
+  font-size: inherit;
+  font-weight: inherit;
+  font-family: inherit;
+  color: inherit;
+  text-align: inherit;
+`;
+
+export default EditableTextField;
